ba7d0bef07b9a6719fa5b246d8d61460b82de6d8
lopuhin <kostia.lopuhin@gmail.com>
2011-02-28 18:08:02 +0300

    let user pass initialization params to redis.Redis

--- qr.py
+++ qr.py
@@ -22,8 +22,6 @@ try:
 except ImportError:
     import simplejson as json
 
-# redis interface
-redis = redis.Redis()
 
 class NullHandler(logging.Handler):
     """A logging handler that discards all logging records"""
@@ -33,146 +31,157 @@ class NullHandler(logging.Handler):
 # Clients can add handlers if they are interested.
 log = logging.getLogger('qr')
 log.addHandler(NullHandler())
+
+
+class UsingRedisMixin(object):
+    """Hold a reference to redis. Let user pass initialization params to Redis"""
+    def __init__(self, redis_init_kwargs = None):
+        self.redis = redis.Redis(**(redis_init_kwargs or {}))
+
 	
-class Deque(object):
+class Deque(UsingRedisMixin):
     """Implements a double-ended queue"""
 
-    def __init__(self, key):
+    def __init__(self, key, redis_init_kwargs = None):
         self.key = key
+        super(Deque, self).__init__(redis_init_kwargs)
 
     def pushback(self, element):
         """Push an element to the back"""
         key = self.key
-        push_it = redis.lpush(key, element)
+        push_it = self.redis.lpush(key, element)
         log.debug('Pushed ** %s ** for key ** %s **' % (element, key))
 		
     def pushfront(self, element):
         """Push an element to the front"""
         key = self.key
-        push_it = redis.rpush(key, element)
+        push_it = self.redis.rpush(key, element)
         log.debug('Pushed ** %s ** for key ** %s **' % (element, key))
 
     def popfront(self):
         """Pop an element from the front"""
         key = self.key
-        popped = redis.rpop(key)
+        popped = self.redis.rpop(key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, key))
         return popped 
 
     def popback(self):
         """Pop an element from the back"""
         key = self.key
-        popped = redis.lpop(key)
+        popped = self.redis.lpop(key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, key))
         return popped 
 
     def elements(self):
         """Return all elements as a Python list"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1)
+        all_elements = self.redis.lrange(key, 0, -1)
         return all_elements
 				
     def elements_as_json(self):
         """Return all elements as a JSON object"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1)
+        all_elements = self.redis.lrange(key, 0, -1)
         all_elements_as_json = json.dumps(all_elements)
         return all_elements_as_json
 
-class Queue(object):	
+class Queue(UsingRedisMixin):	
     """Implements a FIFO queue"""
 
-    def __init__(self, key):
+    def __init__(self, key, redis_init_kwargs = None):
         self.key = key
+        super(Queue, self).__init__(redis_init_kwargs)
 	
     def push(self, element):
         """Push an element"""
         key = self.key
-        push_it = redis.lpush(key, element)
+        push_it = self.redis.lpush(key, element)
         log.debug('Pushed ** %s ** for key ** %s **' % (element, key))
 
     def pop(self):
         """Pop an element"""
         key = self.key
-        popped = redis.rpop(key)
+        popped = self.redis.rpop(key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, key))
         return popped 	
 
     def elements(self):
         """Return all elements as a Python list"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1) or [ ]
+        all_elements = self.redis.lrange(key, 0, -1) or [ ]
         return all_elements
 				
     def elements_as_json(self):
         """Return all elements as a JSON object"""	
         key = self.key
-        all_elements = redis.lrange(key, 0, -1) or [ ]
+        all_elements = self.redis.lrange(key, 0, -1) or [ ]
         all_elements_as_json = json.dumps(all_elements)
         return all_elements_as_json
 
-class CappedCollection(object):
+class CappedCollection(UsingRedisMixin):
     """Implements a capped collection (the collection never
     gets larger than the specified size)."""
 
-    def __init__(self, key, size):
+    def __init__(self, key, size, redis_init_kwargs = None):
         self.key = key
         self.size = size
+        super(CappedCollection, self).__init__(redis_init_kwargs)
 
     def push(self, element):
         key = self.key
         size = self.size
-        pipe = redis.pipeline() # Use multi-exec command via redis-py pipelining
+        pipe = self.redis.pipeline() # Use multi-exec command via redis-py pipelining
         pipe = pipe.lpush(key, element).ltrim(key, 0, size-1) # ltrim is zero-indexed 
         pipe.execute()
 
     def pop(self):
         key = self.key
-        popped = redis.rpop(key)
+        popped = self.redis.rpop(key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, key))
         return popped
 
     def elements(self):
         """Return all elements as Python list"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1)   
+        all_elements = self.redis.lrange(key, 0, -1)   
         return all_elements
 
     def elements_as_json(self):
         """Return all elements as JSON object"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1)
+        all_elements = self.redis.lrange(key, 0, -1)
         all_elements_as_json = json.dumps(all_elements)
         return all_elements_as_json
 
-class Stack(object):
+class Stack(UsingRedisMixin):
     """Implements a LIFO stack""" 
 
-    def __init__(self, key):
+    def __init__(self, key, redis_init_kwargs = None):
         self.key = key
+        super(Stack, self).__init__(redis_init_kwargs)
 
     def push(self, element):
         """Push an element"""
         key = self.key
-        push_it = redis.lpush(key, element)
+        push_it = self.redis.lpush(key, element)
         log.debug('Pushed ** %s ** for key ** %s **' % (element, key))
 		 
     def pop(self):
         """Pop an element"""
         key = self.key
-        popped = redis.lpop(key)
+        popped = self.redis.lpop(key)
         log.debug('Popped ** %s ** from key ** %s **' % (popped, key))
         return popped 
 	
     def elements(self):
         """Return all elements as Python list"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1)   
+        all_elements = self.redis.lrange(key, 0, -1)   
         return all_elements
 
     def elements_as_json(self):
         """Return all elements as JSON object"""
         key = self.key
-        all_elements = redis.lrange(key, 0, -1)
+        all_elements = self.redis.lrange(key, 0, -1)
         all_elements_as_json = json.dumps(all_elements)
         return all_elements_as_json
